303d199278802a9b6ad5b1ec9df318819380141d
Frode <frode@fintlabs.no>
2020-01-21 12:43:18 +0100

    added user api

---
+++ src/Controller/UserController.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class UserController
+{
+
+    public function getUsers() {
+        return new JsonResponse(
+            array(
+                array(
+                    "username" => "finn",
+                    "passwrod" => "finn12",
+                    "fullName" => "Finn Gundersen"
+                ),
+                array(
+                    "username" => "patrick",
+                    "passwrod" => "patrick12",
+                    "fullName" => "Patrick Lorentzen"
+                ),
+                array(
+                    "username" => "michael",
+                    "passwrod" => "michael12",
+                    "fullName" => "Micael Rønnevik"
+                )
+            )
+        );
+    }
+}
